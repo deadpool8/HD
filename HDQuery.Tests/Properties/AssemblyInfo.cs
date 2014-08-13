9323a8809a51b66bccab5e992d13315f5fae2c0d
jonjoaquin <jonjoaquin8@gmail.com>
2014-08-13 08:34:04 -0500

    Baseline

---
+++ HDQuery.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HDQuery.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Benefitmall")]
+[assembly: AssemblyProduct("HDQuery.Tests")]
+[assembly: AssemblyCopyright("Copyright © Benefitmall 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1140334f-ed5c-4eaf-a6c5-92f499d007d5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HDQuery/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using Ninject;
+using System.Web.Mvc;
+
+namespace HDQuery.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+
+        public NinjectControllerFactory()
+        {
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+        protected override IController GetControllerInstance(RequestContext
+        requestContext, Type controllerType)
+        {
+            return controllerType == null
+            ? null
+            : (IController)ninjectKernel.Get(controllerType);
+        }
+        private void AddBindings()
+        {
+            //ninjectKernel.Bind<IProductRepository>().To<EFProductRepository>();
+            //EmailSettings emailSettings = new EmailSettings
+            //{
+            //    WriteAsFile = bool.Parse(ConfigurationManager
+            //    .AppSettings["Email.WriteAsFile"] ?? "false")
+            //};
+
+            //ninjectKernel.Bind<IOrderProcessor>()
+            //.To<EmailOrderProcessor>()
+            //.WithConstructorArgument("settings", emailSettings);
+
+            //ninjectKernel.Bind<IAuthProvider>().To<FormsAuthProvider>();
+        }
+    }
+}
